b7f09be89f27a8e172da0108c846dcbc5e434dbf
Shreshth Awasthi <shreshthawathiab@gmail.com>
2024-06-02 15:09:47 +0530

    changes

--- back-end/config/db.js
+++ back-end/config/db.js
@@ -1,5 +1,5 @@
 import mongoose from "mongoose";
 
 export const connectDB=async ()=>{
-    await mongoose.connect('mongodb+srv://shreshthawathiab:Shreshth137@cluster0.jkbrffh.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0').then(console.log('connected to db'))
+    await mongoose.connect(process.env.DBHOST).then(console.log('connected to db'))
 }
